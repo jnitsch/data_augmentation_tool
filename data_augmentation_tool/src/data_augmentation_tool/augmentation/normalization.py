fixing bug in normalization (due to integer division)

data_augmentation_tool/src/data_augmentation_tool/augmentation/normalization.py
@@ -1,4 +1,4 @@
-__author__ = 'jnitsch'
+__author__ = 'Julia Nitsch'
 """@package data_augmentation_tool.augmentation.normalization
 Functions to normalize images
 
@@ -6,7 +6,7 @@ Different functions for normalizing images are implemented here
 """
 
 import numpy as np
-
+import cv2
 
 def std_normalization(patch):
     """Normalizes patch
@@ -17,12 +17,16 @@ def std_normalization(patch):
     """
 
     # normalize from 0 to 1
-    img = np.divide(patch, 255)
+    img = np.divide(patch, 255.0)
 
     # normalize for mean
-    img = img - np.mean(img)
+    mean, std = cv2.meanStdDev(img)
+    img = img - mean
 
     # normalize for standard dev
-    img = np.divide(img, np.std(img))
+    if np.isnan(std).any():
+        std = 1.0
+
+    img = np.divide(img, std)
 
     return img